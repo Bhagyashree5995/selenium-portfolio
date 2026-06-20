b115d6d1e7809a5645c917b6a1231fcb97a6c289
Bhagyashree Manjithaya <bhagyashreemanjithaya@Kaushiks-Mac-mini.local>
2026-06-20 10:35:01 +0200

    Fix CI driver setup and add missing navigation in LoginPage

--- conftest.py
+++ conftest.py
@@ -1,7 +1,5 @@
 import pytest
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
 
 @pytest.fixture(scope="function")
 def driver():
@@ -9,10 +7,7 @@ def driver():
     options.add_argument("--headless")
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
-    driver = webdriver.Chrome(
-        service=Service(ChromeDriverManager().install()),
-        options=options
-    )
+    driver = webdriver.Chrome(options=options)
     driver.implicitly_wait(10)
     yield driver
     driver.quit()
--- tests/pages/login_page.py
+++ tests/pages/login_page.py
@@ -4,6 +4,7 @@ from selenium.webdriver.common.by import By
 class LoginPage:
     def __init__(self, driver):
         self.driver = driver
+        self.driver.get("https://the-internet.herokuapp.com/login")
 
     def enter_username(self, username):
         self.driver.find_element(By.ID, "username").send_keys(username)
